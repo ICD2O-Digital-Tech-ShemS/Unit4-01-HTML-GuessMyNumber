4b18655483e713007575befa2a3540cd8e16e8a4
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-05-01 14:12:58 +0000

    update

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict";
+function Guess() {
+    let ans= "";
+    let guessNumber = parseInt(document.getElementById("guessNumber").value);
+    if (guessNumber == 12) {
+        ans = "You got the Answer"
+    }
+    if (guessNumber != 12) {
+        ans = "You got it wrong. the correct answer is" + 12; 
+    }
+    document.getElementById("ans").innerHTML = ans ;
+}
